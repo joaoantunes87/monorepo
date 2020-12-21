a2c8c7cfc4272db03f9f58246e9f8bca371b60ad
João Antunes <joao.antunes@mobiweb.com>
2020-12-21 15:55:41 +0000

    SPA entrypoint

--- packages/spa/src/index.js
+++ packages/spa/src/index.js
@@ -1,34 +1,7 @@
 import "./index.css";
 
-// import React from "react";
-import { AppRegistry /* , StyleSheet, Text, View */ } from "react-native";
-// import { BookCard } from "@mr/ui-shared";
+import { AppRegistry } from "react-native";
 import App from "./App";
-
-/*
-class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.box}>
-        <Text style={styles.text}>Hello, world!</Text>
-        <BookCard
-          book={{
-            id: "1",
-            title: "clean code",
-            author: "bob",
-            tag: "software",
-          }}
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  box: { padding: 10 },
-  text: { fontWeight: "bold" },
-});
-*/
 
 AppRegistry.registerComponent("App", () => App);
 AppRegistry.runApplication("App", {
