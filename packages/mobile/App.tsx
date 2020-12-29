ecfaa84e6ce8cc82991f2a91e7d5c093c42018a1
João Antunes <joao.antunes@mobiweb.com>
2020-12-29 20:17:39 +0000

    React navigation

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,23 +1,54 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, StatusBar} from 'react-native';
-// import {NavigationContainer} from '@react-navigation/native';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  View,
+  Pressable,
+} from 'react-native';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import {allBooks} from '@mr/utils';
 import {IBook} from '@mr/types';
 import {BookCard} from '@mr/ui-shared';
 
+function HomeScreen() {
+  const {navigate} = useNavigation();
+
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Pressable
+        onPress={() => {
+          navigate('Books');
+        }}>
+        <Text>Go to Books</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const Books = () => {
+  return (
+    <ScrollView contentInsetAdjustmentBehavior="automatic">
+      {allBooks().map(function renderBook(book: IBook): JSX.Element {
+        return <BookCard key={book.id} book={book} />;
+      })}
+    </ScrollView>
+  );
+};
+
+const Stack = createStackNavigator();
+
 const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          {allBooks().map(function renderBook(book: IBook): JSX.Element {
-            return <BookCard key={book.id} book={book} />;
-          })}
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Books" component={Books} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
